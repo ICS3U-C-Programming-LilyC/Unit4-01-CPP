664f21229d3d579549de97e6c22a6dda73e2ccaa
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-11-04 17:33:15 +0000

    Fixed decimal problem.

--- sumOfNumbers.cpp
+++ sumOfNumbers.cpp
@@ -9,35 +9,58 @@
 #include <iostream>
 
 int main() {
-    // Initializing the sum, counter and user number.
-    int sum = 0;
+    // Declaring variables.
     int counter = 0;
-    std::string user_number_as_string;
+    int sum = 0;
+    int userNumber;
+    // Created a variable for if the user inputs
+    //a float instead of a whole number.
+    float userNumberFloat;
+    std::string userNumberString;
+
+    // Explaining my program to the user.
+    std::cout << 
+    "This program will calculate the sum of the number that you enter.\n";
 
     // Getting user input.
-    std::cout << "Enter a positive number: ";
-    std::cin >> user_number_as_string;
+    std::cout
+        << "Enter a positive integer: ";
+    std::cin >> userNumberString;
 
     // Initiating Try Catch.
     try {
-        int user_number_as_integer = std::stoi(user_number_as_string);
+        // Converting a float into a string.
+        userNumberFloat = std::stof(userNumberString);
 
-        // If statement to check for negatives.
-        if (user_number_as_integer <= 0) {
+        // Converting a string into an integer.
+        userNumber = std::stoi(userNumberString);
+
+        // If statement for decimal input.
+        if (userNumberFloat != userNumber) {
+            std::cout << "Invalid input.";
+
+            // Else if statement for negative number input.
+        } else if (userNumber <= 0) {
             std::cout << "Please enter a positive number.";
+
+            // Else statement for valid input.
         } else {
-            while (counter <= user_number_as_integer) {
+            while (counter <= userNumber) {
+                std::cout << "Tracking " << counter << " times through the loop." << std::endl;
+
+                // Calculating the sum of the user's number.
                 sum = sum + counter;
-                std::cout << "Tracking " << counter
-                << " times through the loop.\n";
+
+                // Incrementing the counter each time.
                 counter = counter + 1;
             }
 
-            // Displaying the sum back to the user.
-            std::cout << "The sum of your number "
-            << user_number_as_integer << " is "
-            << sum;
+            // Displays the sum of the user's number back to them.
+            std::cout << "The sum of your number " << userNumber
+                      << " is " << sum;
         }
+
+        // Catching errors or any invalid inputs.
     } catch (std::invalid_argument) {
         std::cout << "Invalid input.";
     }
